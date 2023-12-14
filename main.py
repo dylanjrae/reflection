f4616188a913445d4763a104adebb50cd30af096
Dylan Rae <55325297+dylanjrae@users.noreply.github.com>
2023-12-14 10:19:26 -0800

    upload local files that are missing from backup

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from merge.core.api_error import ApiError
 from merge.resources.filestorage.types import PaginatedFileList
 from merge.resources.filestorage.types.folder_request import FolderRequest
 from merge.resources.filestorage.types.folder import Folder
+from merge.resources.filestorage.types.file import File
 from merge.resources.filestorage.types.file_storage_file_response import (
     FileStorageFileResponse,
 )
@@ -63,6 +64,72 @@ def create_backup_folder_if_not_exists(merge_client: Merge):
         log.debug("Backup folder already exists!")
 
 
+def get_all_files_in_backup_folder(merge_client: Merge, folder_id: str) -> list[File]:
+    all_files = []
+    try:
+        # Get all files in the current folder
+        files: list[File] = merge_client.filestorage.files.list(
+            folder_id=folder_id
+        ).results
+        all_files.extend(files)
+
+        # Get all subfolders in the current folder
+        folders: list[Folder] = merge_client.filestorage.folders.list(
+            parent_folder_id=folder_id
+        ).results
+
+        # Recursively fetch files in each subfolder
+        for folder in folders:
+            all_files.extend(get_all_files_in_backup_folder(merge_client, folder.id))
+    except ApiError as e:
+        log.error(e.body)
+
+    return all_files
+
+
+def get_all_local_files(dirs: list[str]) -> list[str]:
+    all_files = []
+    for dir in dirs:
+        for root, dirs, files in os.walk(dir):
+            for file in files:
+                all_files.append(os.path.join(root, file))
+    return all_files
+
+
+def find_missing_files(merge_files: list[File], local_files: list[str]) -> list[str]:
+    missing_files = []
+    for local_file in local_files:
+        if not any(merge_file.name == local_file for merge_file in merge_files):
+            missing_files.append(local_file)
+    return missing_files
+
+
+def upload_files(merge_client: Merge, folder_id: str, files: list[str]):
+    for file in files:
+        try:
+            # TODO need to add a file_url to upload from --> maybe a future project is a secure locally hosted python file url system?
+            file_create_result: FileStorageFileResponse = (
+                merge_client.filestorage.files.create(
+                    model=File(name=file, folder_id=folder_id)
+                )
+            )
+        except ApiError as e:
+            log.error(e.body)
+        log.debug(f"Uploaded {file}!")
+
+
+def upload_all_missing_files(merge_client: Merge, dirs: list[str]):
+    # get current files in backup folder
+    # get folder id
+    backup_folder: Folder = merge_client.filestorage.folders.list(
+        name="reflection-backup"
+    ).results[0]
+    merge_files = get_all_files_in_backup_folder(merge_client, backup_folder.id)
+    local_files = get_all_local_files(dirs)
+    files_to_upload = find_missing_files(merge_files, local_files)
+    upload_files(merge_client, backup_folder.id, files_to_upload)
+
+
 def main():
     log.basicConfig(level=log.DEBUG, format="%(message)s")
 
@@ -79,6 +146,7 @@ def main():
     drive_merge_client = create_drive_merge_client()
     verify_account_link(drive_merge_client)
     create_backup_folder_if_not_exists(drive_merge_client)
+    upload_all_missing_files(drive_merge_client, dirs)
 
     # files: PaginatedFileList = merge_client.filestorage.files.list()
     # log.debug(files.json())
